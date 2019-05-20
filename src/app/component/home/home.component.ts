0c8571296d0fe0285db4a52ba527a92e4d3b7f1a
marco.pollacci <marco.pollacci@lepida.it>
2019-05-20 14:20:54 +0200

    Porting Site to Angular

---
+++ src/app/component/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  year: number;
+
+  constructor() { }
+
+  ngOnInit() {
+
+    const datenow = new Date();
+    this.year = datenow.getFullYear();
+  }
+
+}
