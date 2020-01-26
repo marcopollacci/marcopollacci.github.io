af4aa13968d727cad461680b184cabaf35066d7d
Marco <pollacci@gmail.com>
2020-01-26 21:42:19 +0100

    aggiornamento routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,12 @@ import { HomeComponent } from './component/home/home.component';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     component: HomeComponent
   },
   {
     path: '**',
-    redirectTo: 'home'
+    redirectTo: ''
   }
 ];
 
